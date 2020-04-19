dac864fa03cc1454e4535017785609bf00693e53
Valeriy-lynnyk <63010316+Valeriy-lynnyk@users.noreply.github.com>
2020-04-19 17:33:21 +0200

    Update holaworld.java

--- prueba/src/prueba/holaworld.java
+++ prueba/src/prueba/holaworld.java
@@ -4,7 +4,7 @@ public class holaworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("holamundo");
+		System.out.println("hndo");
 	}
 
 }
